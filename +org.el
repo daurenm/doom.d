370f31a3f88d7dc06b9bb37d44578abd405cf207
Dauren <dauren.ktl@gmail.com>
2022-05-07 23:18:01 +0100

    add a custom dates format

--- +org.el
+++ +org.el
@@ -12,5 +12,12 @@
                          (org-forward-element)
                        (evil-next-visual-line)))))
 
+;; European format for dates
+;; (setq org-time-stamp-custom-formats '("<%d/%m/%y %a>" . "<%d/%m/%y %a %H:%M>"))
+(setq org-time-stamp-custom-formats '("<%d.%m %a>" . "<%d.%m %a %H:%M>"))
+(setq org-display-custom-times t)
+
 (after! org
-  (setq org-todo-keywords '((sequence "TODO(t)" "HOLD(h)" "|" "DONE(d)" "KILL(k)"))))
+  (setq org-todo-keywords '((sequence "TODO(t)" "HOLD(h)" "|" "DONE(d)" "KILL(k)")))
+  (map! :n ", a" #'org-agenda
+        :map general-override-mode-map "C-c C-w" #'org-check-deadlines))
